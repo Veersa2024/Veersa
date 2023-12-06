6c924918102a8d407bf5b82858047b0c329c5e01
Veersa2024 <Manya@veersatech.com>
2023-12-06 12:37:33 +0530

    changes done

--- cypress/e2e/veersa_url4.cy.js
+++ cypress/e2e/veersa_url4.cy.js
@@ -5,7 +5,7 @@ describe('Accessibility Tests', () => {
     it('url4', () => {
         // Perform accessibility checks after the page has loaded
         cy.visit('https://veersatech.com/careers-2/', { failOnStatusCode: false });
-       
+    //    making some changes
         cy.injectAxe();
         cy.checkA11y();
     });
