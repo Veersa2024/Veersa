7f3b46f6dc1c4cef6e184ff51467b66346067529
Veersa2024 <Manya@veersatech.com>
2023-12-06 12:57:07 +0530

    changes done4

--- cypress/e2e/veersa_url2.cy.js
+++ cypress/e2e/veersa_url2.cy.js
@@ -4,7 +4,7 @@ describe('Accessibility Tests', () => {
     it('url2', () => {
 
         cy.visit('https://veersatech.com/products-services/', { failOnStatusCode: false });
-       
+       cy.wait(10)
         cy.injectAxe();
         cy.checkA11y();
     });
