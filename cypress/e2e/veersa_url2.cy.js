5d5cb5eeaf0dd6a4e3d7851372ff8e282f530774
Veersa2024 <Manya@veersatech.com>
2023-12-06 12:47:47 +0530

    changes done 2

--- cypress/e2e/veersa_url2.cy.js
+++ cypress/e2e/veersa_url2.cy.js
@@ -2,16 +2,10 @@ import 'cypress-xpath'
 describe('Accessibility Tests', () => {
     
     it('url2', () => {
-<<<<<<< HEAD
         // Perform accessibility checks after the page has loaded
         cy.visit('https://veersatech.com/products-services/', { failOnStatusCode: false });
         // cy.get("ul#menu-header-menu > li >a").contains('Products & Services').click({ force: true })
         // Inject axe-core into the application
-=======
-
-        cy.visit('https://veersatech.com/products-services/', { failOnStatusCode: false });
-   
->>>>>>> 0dbae0812f1e2832692ba6f983013f60f2bf6edf
         cy.injectAxe();
         cy.checkA11y();
     });
--- cypress/e2e/veersa_url4.cy.js
+++ cypress/e2e/veersa_url4.cy.js
@@ -4,14 +4,9 @@ describe('Accessibility Tests', () => {
 
     it('url4', () => {
         // Perform accessibility checks after the page has loaded
-<<<<<<< HEAD
         cy.visit('https://veersatech.com/careers-2/', { failOnStatusCode: false });
     //    making some changes
-=======
-
-        cy.visit('https://veersatech.com/careers-2/', { failOnStatusCode: false });
-
->>>>>>> 0dbae0812f1e2832692ba6f983013f60f2bf6edf
+    cy.wait(10)
         cy.injectAxe();
         cy.checkA11y();
     });
