0e3947e9934f65d8b5fb82a3925dc0e664c322a5
Veersa2024 <152957655+Veersa2024@users.noreply.github.com>
2023-12-06 11:46:36 +0530

    Update veersa_url5.cy.js

--- cypress/e2e/veersa_url5.cy.js
+++ cypress/e2e/veersa_url5.cy.js
@@ -4,7 +4,7 @@ describe('Accessibility Tests', () => {
 
     it('url5', () => {
         // Perform accessibility checks after the page has loaded
-        cy.visit('https://veersatech.com/');
+        cy.visit('https://veersatech.com/',{ failOnStatusCode: false });
         cy.get("ul#menu-header-menu > li >a").contains('Blog').click({ force: true })
         // Inject axe-core into the application
         cy.injectAxe();
