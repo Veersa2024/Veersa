c47930feb9a8dcf452d9cbb472e8ddbba1150927
Veersa2024 <152957655+Veersa2024@users.noreply.github.com>
2023-12-06 12:44:44 +0530

    Update veersa_url5.cy.js

--- cypress/e2e/veersa_url5.cy.js
+++ cypress/e2e/veersa_url5.cy.js
@@ -3,14 +3,9 @@ describe('Accessibility Tests', () => {
     
 
     it('url5', () => {
-<<<<<<< HEAD
-        cy.visit('https://veersatech.com/blog-esg-initiatives/', { failOnStatusCode: false });
-     
-=======
-   
-        cy.visit('https://veersatech.com/blog-esg-initiatives/', { failOnStatusCode: false });
 
->>>>>>> 0dbae0812f1e2832692ba6f983013f60f2bf6edf
+        cy.visit('https://veersatech.com/blog-esg-initiatives/', { failOnStatusCode: false });
+  
         cy.injectAxe();
         cy.checkA11y();
     });
