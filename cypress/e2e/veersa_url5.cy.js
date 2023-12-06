cce0152528e593394544d3470335d4cbd8e99e0d
Veersa2024 <Manya@veersatech.com>
2023-12-06 12:34:54 +0530

    changes done

--- cypress/e2e/veersa_url5.cy.js
+++ cypress/e2e/veersa_url5.cy.js
@@ -3,14 +3,8 @@ describe('Accessibility Tests', () => {
     
 
     it('url5', () => {
-        // Perform accessibility checks after the page has loaded
-<<<<<<< HEAD
-        cy.visit('https://veersatech.com/', { failOnStatusCode: false });
-=======
-        cy.visit('https://veersatech.com/',{ failOnStatusCode: false });
->>>>>>> e39e4daf89654c76c128682f559e899f30e804ef
-        cy.get("ul#menu-header-menu > li >a").contains('Blog').click({ force: true })
-        // Inject axe-core into the application
+        cy.visit('https://veersatech.com/blog-esg-initiatives/', { failOnStatusCode: false });
+     
         cy.injectAxe();
         cy.checkA11y();
     });
